92f5f31fc43b191a4b5c6559cfb1e745cc30a373
Soapalin <lucien.a.tran@gmail.com>
2024-08-03 23:38:14 +1000

    added algorithm class

--- python/artifact_connect.py
+++ python/artifact_connect.py
@@ -1,12 +1,17 @@
-import requests 
+from enum import Enum 
+from player import Player 
+class CharacterType(Enum):
+    FIGHTER = 0 
+    GATHERER = 1
+    CRAFTER = 2 
 
-class ArtefactConnect():
+class ArtifactAlgorithm(Player):
     def __init__(self, jwt):
         self.jwt = jwt 
         self.server = "https://api.artifactsmmo.com"
         print(self.server)
 
-    def move():
+    def fighter():
         pass
 
 
--- python/player.py
+++ python/player.py
@@ -1,22 +1,34 @@
 import requests
 import datetime
-from artifact_connect import ArtefactConnect
 
-class Player(ArtefactConnect):
+class Player():
     def __init__(self, name, jwt):
         self.name = name
-        self.request = ArtefactConnect.__init__(self, jwt=jwt)
+        self.jwt = jwt 
         self.auth_header = {'Authorization': "Bearer " + self.jwt, 'Content-Type': 'application/json', 'Accept': 'application/json'}
         self.cooldown = 0
+        self.server = "https://api.artifactsmmo.com" 
         self.last_updated = datetime.datetime.now()
 
     def move(self, x, y):
         body = "{" + f"\"x\": \"{x}\",\"y\": \"{y}\""  + "}"
-        # body = {'x': x, 'y': y}
-        move_api = f"https://api.artifactsmmo.com/my/{self.name}/action/move"
+        move_api = f"{self.server}/my/{self.name}/action/move"
         r = requests.post(move_api, data=body, headers=self.auth_header)
         print(r.status_code)
         if r.status_code == 200:
-            # print(r.json())
             response = r.json()["data"]
-            print(response["cooldown"])
+            print(response)
+            self.updateCooldown(response["cooldown"]["remaining_seconds"])
+            return r.json()["data"]
+        else:
+            return r.json()
+            
+
+    def updateCooldown(self, cooldown):
+        self.last_updated = datetime.datetime.now()
+        self.cooldown = cooldown
+
+
+    def fight(self):
+        body = f""
+        fight_api = f"{self.server}/my/{self.name}/action/fight"
