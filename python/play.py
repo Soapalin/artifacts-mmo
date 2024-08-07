8d53d4d27ba05bcca21b99ae1bc964531d29d80f
Soapalin <lucien.a.tran@gmail.com>
2024-08-07 22:11:44 +1000

    removed shrimp fishing as too high lvl

--- python/play.py
+++ python/play.py
@@ -65,20 +65,20 @@ while True:
                 print(f"Fished gudgeon fishing spot and waited for cooldown: {response['cooldown']['remaining_seconds'] + 1}s")
 
 
-        rinsyi.move(5,2) # shrimp fishing spot
-        for i in range(30):
-            status_code, response = rinsyi.gathering()
-            if status_code != 200:
-                print(f"Failed to gather: {response}")
-                if status_code == 497:
-                    print(f"Inventory full. Batch deposit all items to bank.")
-                    # rinsyi.deposit_item_to_bank(item_code="chicken", )
-                    rinsyi.move_to_closest_bank()
-                    rinsyi.batch_deposit_items_to_bank()
-                    rinsyi.deposit_gold_to_bank()
-                    rinsyi.move(5,2)
-            else:
-                print(f"Fished shrimp fishing spot and waited for cooldown: {response['cooldown']['remaining_seconds'] + 1}s")
+        # rinsyi.move(5,2) # shrimp fishing spot need lvl 10
+        # for i in range(30):
+        #     status_code, response = rinsyi.gathering()
+        #     if status_code != 200:
+        #         print(f"Failed to gather: {response}")
+        #         if status_code == 497:
+        #             print(f"Inventory full. Batch deposit all items to bank.")
+        #             # rinsyi.deposit_item_to_bank(item_code="chicken", )
+        #             rinsyi.move_to_closest_bank()
+        #             rinsyi.batch_deposit_items_to_bank()
+        #             rinsyi.deposit_gold_to_bank()
+        #             rinsyi.move(5,2)
+        #     else:
+        #         print(f"Fished shrimp fishing spot and waited for cooldown: {response['cooldown']['remaining_seconds'] + 1}s")
 
         rinsyi.move(0,1) # chicken
         for i in range(60):
